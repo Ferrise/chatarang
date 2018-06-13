d18cef24f0ef2919c69d7d971fcd10223489877d
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-13 07:29:33 -0400

    Begin styling message form

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -18,12 +18,14 @@ class MessageForm extends Component {
         return (
             <form 
             className="MessageForm" 
+            style = {styles.messageForm}
             onSubmit = {this.handleSubmit} 
             >
                 <input 
                     type="text" 
                     name="body" 
                     placeholder="Type a message..." 
+                    style = {styles.inputChildren}
                     value={this.state.body}
                     onChange={this.handleChange}
                 />
@@ -31,6 +33,25 @@ class MessageForm extends Component {
             </form>
         )
     }
+}
+
+const styles = {
+    messageForm: {
+        backgroundColor: 'white',
+        height: '3rem',
+        display: 'flex',
+        alignItems: 'stretch',
+        border: '2px solid #999',
+        borderRadius: '0.5rem',
+        margin: '0.25rem',
+        padding: 0,
+      },
+      
+      inputChildren: {
+        flex: 1,
+        fontSize: '1.2rem',
+        border: 0,
+      },
 }
 
 export default MessageForm
