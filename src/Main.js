42e4539c40a9cbedea30fc1e2c3923e761ff39ad
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-14 07:05:45 -0400

    Link channels and create function to change room

--- src/Main.js
+++ src/Main.js
@@ -4,11 +4,21 @@ import Sidebar from './Sidebar'
 import Chat from './Chat'
 
 class Main extends Component {
+  state = {
+    room: 'general'
+  }
+  
+  changeRoom = (room) => {
+    debugger
+    this.setState({ room })
+    debugger
+  }
+
   render() {
     return (
       <div className="Main" style={styles}>
-        <Sidebar user={this.props.user} signOut={this.props.signOut} />
-        <Chat user={this.props.user} />
+        <Sidebar user={this.props.user} changeRoom={this.changeRoom} signOut={this.props.signOut} />
+        <Chat user={this.props.user} room={this.state.room}/>
       </div>
     )
   }
