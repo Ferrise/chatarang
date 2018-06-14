31372712ab79db92ca1822d5cc1a26a7467dd9e5
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-14 04:35:35 -0400

    Modify style in chat header and chat components

--- src/Chat.js
+++ src/Chat.js
@@ -5,48 +5,43 @@ import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
 class Chat extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super()
 
-        this.state = {
-            messages: [
-                {id: 1, userName: 'dstrus', body: 'Chatting up a strom, yo!'},
-                {id: 2, userName: 'dpalazzo', body: 'This guy is so annoying. I hate my job.'},
-            ],
-        }
+    this.state = {
+      messages: [
+        { id: 1, userName: 'dstrus', body: 'Chatting up a storm, yo!' },
+        { id: 2, userName: 'dpalazzo', body: 'This guy is so annoying. I hate my job.' },
+      ],
     }
+  }
 
-    addMessage = (body) => {
-        const messages = [...this.state.messages]
-        messages.push({
-            id: Date.now(),
-            userName: this.props.user,
-            body,
-        })
-        
-        this.setState({ messages })
-    }
+  addMessage = (body) => {
+    const messages = [...this.state.messages]
+    messages.push({
+      id: Date.now(),
+      userName: this.props.user.userName,
+      body,
+    })
 
-    render() {
-        return (
-            <div 
-                className="Chat" 
-                style = {styles.chat}
-            >
-                <ChatHeader />
-                <MessageList messages={this.state.messages} user ={this.props.user}/>
-                <MessageForm addMessage = {this.addMessage}/>
-            </div>
-        )
-    }
+    this.setState({ messages })
+  }
+
+  render() {
+    return (
+      <div className="Chat" style={styles}>
+        <ChatHeader />
+        <MessageList messages={this.state.messages} />
+        <MessageForm addMessage={this.addMessage} />
+      </div>
+    )
+  }
 }
 
 const styles = {
-    chat: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'column',
-      },
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
 }
 
 export default Chat
