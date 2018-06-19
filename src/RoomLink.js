cb6039d6cc536327cf993cede1db0f10c1e5bb07
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-18 21:16:37 -0400

    Use NavLink to change rooms

--- src/RoomLink.js
+++ src/RoomLink.js
@@ -1,43 +1,38 @@
 import React from 'react'
-import {StyleSheet, css} from 'aphrodite'
+import { StyleSheet, css } from 'aphrodite'
+import { NavLink } from 'react-router-dom'
 
-const RoomLink = ({ room, loadRoom }) => {
-    const handleClick = (ev) => {
-        ev.preventDefault()
-        loadRoom(room)
-    }
-
-    return(
-        <li className={css(styles.item)}>
-          <a 
-            href="./index.html" 
-            className={css(styles.link)} 
-            onClick = { handleClick }>
-            { room.name }
-          </a>
-        </li>
-    )
+const RoomLink = ({ room }) => {
+  return (
+    <li className={css(styles.item)}>
+      <NavLink
+        to={`/rooms/${room.name}`}
+        className={css(styles.link)}
+      >
+        { room.name }
+      </NavLink>
+    </li>
+  )
 }
 
 const styles = StyleSheet.create({
-    item: {
-      marginBottom: '0.5rem',
-    },
-  
-    link: {
-      display: 'block',
-      color: 'whitesmoke',
-      textDecoration: 'none',
-  
-      '::before': {
-        content: '"# "',
-      },
-  
-      ':hover': {
-        backgroundColor: 'rgba(255, 255, 255, 0.2)',
-      }
-    },
-  })
+  item: {
+    marginBottom: '0.5rem',
+  },
 
-  export default RoomLink 
-  
+  link: {
+    display: 'block',
+    color: 'whitesmoke',
+    textDecoration: 'none',
+
+    '::before': {
+      content: '"# "',
+    },
+
+    ':hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    }
+  },
+})
+
+export default RoomLink
