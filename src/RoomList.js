2012c20f9068b7dc11f64eb70597f5989dfd48bc
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-20 10:57:17 -0400

    WIP add direct messages

--- src/RoomList.js
+++ src/RoomList.js
@@ -3,7 +3,7 @@ import { StyleSheet, css } from 'aphrodite'
 import { Route, Switch, Link } from 'react-router-dom'
 
 import RoomLink from './RoomLink'
-import RoomForm from './RoomForm'
+import DirectMessageForm from './DirectMessageForm'
 
 class RoomList extends Component {
   render() {
@@ -15,7 +15,7 @@ class RoomList extends Component {
           path="/rooms/new"
           render={
             navProps => (
-              <RoomForm
+              <DirectMessageForm
                 addRoom={this.props.addRoom}
                 users={this.props.users}
                 user={this.props.user}
